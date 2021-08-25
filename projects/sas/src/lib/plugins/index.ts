5b16c276ef37688bc95b595ce582a679559b1cc7
Evgeny Dobryansky <fitz.deve@gmail.com>
2021-08-25 20:50:54 +0300

    observe route

--- projects/sas/src/lib/plugins/index.ts
+++ projects/sas/src/lib/plugins/index.ts
@@ -1,2 +1,5 @@
 export * from './storage/sas-storage-plugin.module';
 export * from './storage/persistence';
+export * from './route-observer/route-observe';
+export * from './route-observer/abstract-route-observer';
+
