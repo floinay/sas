e26b13b6fdee1103354d62bfaaa69279d049b2f3
Evgeny Dobryansky <fitz.deve@gmail.com>
2021-10-18 14:22:03 +0300

    'before-reset'

--- projects/sas/src/lib/actions/sas-actions.module.ts
+++ projects/sas/src/lib/actions/sas-actions.module.ts
@@ -4,6 +4,11 @@ import {ActionsListenerService} from './actions-listener.service';
 import {ActionsInjector} from './actions-injector';
 import {SAS_ACTIONS_PROVIDERS} from './actions.providers';
 
+/**
+ * This module enable actions calls listener.
+ * You don`t need import this module for use @Action decorator.
+ * @Example: You develop plugin that listen actions calls and send to statistics server. You need import SasActionsModule module to root module and after that inject ActionsListenerService and subscribe to listen() method
+ */
 @NgModule({
   providers: [
     ActionsListenerService,
